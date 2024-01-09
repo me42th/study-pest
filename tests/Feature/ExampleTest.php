4e2523d3d08fc68e931f01b05d00d8b211a0e377
me42th <david.meth@hotmail.com>
2024-01-09 06:08:54 -0300

    [lesson 4] first test

--- tests/Feature/ExampleTest.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-it('gives back succeful response for home page',function(){
-    $response = $this->get('/');
-    $response->assertStatus(200);
-});
---
+++ tests/Feature/PageResponseTest.php
@@ -0,0 +1,7 @@
+<?php
+use function Pest\Laravel\get;
+
+it('gives back succeful response for home page',function(){
+    // Act & Assert
+    get(route('home'))->assertOk();
+});
